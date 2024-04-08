1890d11f9d5e834d708ff5056d5548f943c58056
shivanimeena1 <153616308+shivanimeena1@users.noreply.github.com>
2024-04-08 11:31:27 +0630

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function display(val) {
+  document.getElementById('display').value += val;
+}
+
+function calculator() {
+  let x = document.getElementById('display').value;
+  let y = eval(x);
+  document.getElementById('display').value = y.toFixed(2);
+}
+
+function allclear() {
+  let string = "";
+  document.getElementById("display").value = string;
+}
+
+function remove() {
+  let input = document.getElementById("display").value.slice(0, -1);
+  document.getElementById("display").value = input;
+}
+
